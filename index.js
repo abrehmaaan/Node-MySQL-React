858f31a82e1a092700c591c2750c8275b0c7b10d
abrehmaaan <abdulreh4am9a@gmail.com>
2023-07-21 00:49:11 +0500

    feat: data stored to database

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 import express from "express";
 import request from "request";
+import mysql from "mysql";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res)=>{
-    let name = req.query.country;
-    let url = "http://universities.hipolabs.com/search?country=Pakistan";
-    request(url, { json: true }, (err, response, data) => {
-        if (err) { return console.log(err); }
-        let unis = []
-        for(let d of data){
-            unis.push({
-                name: d.name,
-                domain: d.domains[0],
-                country: d.country,
-                country_code: d.alpha_two_code,
-                webpage: d.web_pages[0]
-            })
-            console.log(unis);
-        }
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "accessretail",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to database!");
+});
+
+app.get("/", (req, res) => {
+  let url = "http://universities.hipolabs.com/search?country=Pakistan";
+  request(url, { json: true }, (err, response, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error fetching data");
+    }
+    let unis = [];
+    for (let d of data) {
+      unis.push({
+        name: d.name,
+        domain: d.domains[0],
+        country: d.country,
+        country_code: d.alpha_two_code,
+        webpage: d.web_pages[0],
+      });
+    }
+    console.log(unis);
+
+    const insertQuery = "INSERT INTO universities (name, domain, country, country_code, webpage) VALUES ?";
+    const values = unis.map((uni) => [
+      uni.name,
+      uni.domain,
+      uni.country,
+      uni.country_code,
+      uni.webpage,
+    ]);
+
+    connection.query(insertQuery, [values], (err, result) => {
+      if (err) {
+        console.error("Error inserting data");
+        return res.status(500).send("Error inserting data");
+      }
+      console.log("Data Inserted!");
+      res.status(200).send("Data inserted!");
     });
-    res.end()
-})
+  });
+});
 
-app.listen(8080, ()=>{
-    console.log("Server Started");
-})
+app.listen(8080, () => {
+  console.log("Server Started!");
+});
